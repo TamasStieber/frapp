817ef37fd94ff0cc5c3e27f347ab56b8b3536ec4
tamas.stieber <tamas.stieber@thyssenkrupp-automotive.com>
2023-10-18 13:54:20 +0200

    Create new issue API endpoint

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,15 +1,37 @@
 import prisma from "@/prisma/client";
+import { IssueType, Priority } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 
+const validateIssueType = (value: any) => {
+  return Object.hasOwn(IssueType, value);
+};
+
+const validatePriority = (value: any) => {
+  return Object.hasOwn(Priority, value);
+};
+
 const issueSchema = z.object({
+  projectId: z.number().nonnegative(),
   title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  description: z.string().min(1).max(65535),
+  type: z.custom<IssueType>(validateIssueType),
+  priority: z.custom<Priority>(validatePriority),
+  deadline: z.string().datetime({ offset: true }).optional().nullable(),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
+  const { projectId, title, description, type, priority, deadline } = body;
   if (!body) return NextResponse.json({ status: 400 });
+
+  const project = await prisma.project.findUnique({ where: { id: projectId } });
+
+  if (!project)
+    return NextResponse.json(
+      { error: "Project does not exist!" },
+      { status: 400 }
+    );
 
   const validation = issueSchema.safeParse(body);
 
@@ -18,9 +40,12 @@ export async function POST(request: NextRequest) {
 
   const newIssue = await prisma.issue.create({
     data: {
-      project: body.project,
-      title: body.title,
-      description: body.description,
+      projectId,
+      title,
+      description,
+      type,
+      priority,
+      deadline,
     },
   });
 
--- app/api/types/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-import z from "zod";
-
-z.object({});
-
-export async function POST(request: NextRequest) {
-  const body = await request.json();
-}
